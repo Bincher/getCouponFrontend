27b3a16841507a947fd3f3aa19dc94835e1b584f
seongbin <catsb21@gmail.com>
2025-04-15 16:42:03 +0900

    페이지네이션 적용용

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -6,17 +6,28 @@ import { GetCouponListResponseDto } from '../../apis/response/coupon';
 import ResponseDto from '../../apis/response/Response.dto';
 import { GetCouponListRequest } from '../../apis';
 import CouponItem from '../../components/CouponItem';
+import { usePagination } from '../../hooks';
+import Pagination from '../../components/Pagination';
 
 export default function Main() {
-
-    // function: 네이게이트 함수 //
-    const navigate = useNavigate();
 
     // component: 메인 화면 상단 컴포넌트 //
     const MainTop =()=>{
 
         // state: 쿠폰 리스트 상태 //
         const [couponList, setCouponList] = useState<CouponListItem[]>([]); 
+
+        // state: 페이지네이션 관련 상태 //
+        const{
+            currentPage,
+            setCurrentPage,
+            currentSection,
+            setCurrentSection,
+            viewList,
+            viewPageList,
+            totalSection,
+            setTotalList
+        } = usePagination<CouponListItem>(3);
 
         // function: get coupon list response 처리 함수 //
         const getCouponListResponse =(responseBody: GetCouponListResponseDto | ResponseDto | null)=> {
@@ -27,6 +38,7 @@ export default function Main() {
     
             const {couponList} = responseBody as GetCouponListResponseDto;
             setCouponList(couponList);
+            setTotalList(couponList);
         }
     
         // effect: 첫 마운트 시 실행될 함수 //
@@ -42,11 +54,22 @@ export default function Main() {
                     <div className='main-top-contents-box'>
                         <div className='main-top-contents-title'>{'현재 쿠폰'}</div>
                         <div className='main-top-contents'>
-                            {couponList.map(couponListItem => <CouponItem couponListItem={couponListItem}/>)}
+                            {viewList.map(couponListItem => <CouponItem couponListItem={couponListItem} />)}
+                        </div>
+                    </div>
+                    
+                        <div className='main-bottom-pagination-box'>
+                            <Pagination 
+                            currentPage={currentPage}
+                            currentSection={currentSection}
+                            setCurrentPage={setCurrentPage}
+                            setCurrentSection={setCurrentSection}
+                            viewPageList={viewPageList}
+                            totalSection={totalSection}
+                            />
                         </div>
                     </div>
                 </div>
-            </div>
         )
     }
 
